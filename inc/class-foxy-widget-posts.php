a15c8fed3753eeb01e12902c1e94c64097a1ae30
Puleeno Nguyen <puleeno@gmail.com>
2018-11-27 07:23:35 +0700

    Udpate foxy widget post

--- inc/class-foxy-widget-posts.php
+++ inc/class-foxy-widget-posts.php
@@ -23,11 +23,11 @@ class Foxy_Widget_Posts extends WP_Widget {
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'category' ); ?>"><?php _e( 'Categories' ); ?></label>
-			<select name="category" id="" multiple="multiple" class="widefat">
+			<select name="<?php echo $this->get_field_name( 'category' ); ?>[]" id="<?php echo $this->get_field_id( 'category' ) ?>" multiple="multiple" class="widefat">
 				<?php foreach( get_categories( array(
 					'hide_empty' => false
 				) ) as $category ): ?>
-				<option value="<?php echo $category->term_id ?>"><?php echo $category->name; ?></option>
+				<option value="<?php echo $category->term_id ?>"<?php selected(true, in_array($category->term_id, array_get( $instance, 'category', array(), true ) ) ) ?>><?php echo $category->name; ?></option>
 				<?php endforeach; ?>
 			</select>
 		</p>
@@ -58,6 +58,15 @@ class Foxy_Widget_Posts extends WP_Widget {
 			'post_type'      => 'post',
 			'posts_per_page' => array_get( $instance, 'posts_per_page', 5, true ),
 		);
+		$category = array_get($instance, 'category', false);
+		if ( ! empty( $category ) ) {
+			$post_args['category__in'] = (array)$category;
+		}
+		$category = array_get($instance, 'tag', false);
+		if ( ! empty( $tag ) ) {
+			$post_args['tag__in'] = (array)$category;
+		}
+
 		$use_carousel = isset( $instance['use_carousel'] ) && $instance['use_carousel'] == 'use';
 
 		Foxy::carousel_options( $use_carousel, $args, $instance );
@@ -71,7 +80,7 @@ class Foxy_Widget_Posts extends WP_Widget {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title']; // WPCS: XSS ok.
 		}
 		$layout_args = array(
-			'style'    => 'card',
+			'style'    => array_get( $instance, 'style' ),
 			'carousel' => $use_carousel,
 		);
 		Foxy::post_layout( $layout_args, $posts, array_merge( $args, $instance ) );
