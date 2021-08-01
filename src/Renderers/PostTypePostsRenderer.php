6696bc42847fc3bafc9d59d55576ba2107975779
Puleeno Nguyen <puleeno@gmail.com>
2021-08-01 23:50:05 +0700

    Support post type posts renderer

--- src/Renderers/PostTypePostsRenderer.php
+++ src/Renderers/PostTypePostsRenderer.php
@@ -1,11 +1,75 @@
 <?php
 namespace Jankx\Widget\Renderers;
 
-abstract class PostTypePostsRenderer extends Base
+use WP_Query;
+use Jankx\PostLayout\PostLayoutManager;
+use Jankx\TemplateLoader;
+use Jankx\PostLayout\Layout\Card;
+
+class PostTypePostsRenderer extends Base
 {
-    protected $postType = 'post';
+    protected $featuredMetaKey;
+    protected $featuredMetaValue;
+
+    protected $options = array(
+        'layout' => 'card',
+        'post_type' => 'post',
+        'posts_per_page' => 10,
+    );
+    protected $layoutOptions = array(
+        'columns' => 4
+    );
+
+    public function setFeaturedMetaKey($key)
+    {
+        if ($key) {
+            $this->featuredMetaKey = $key;
+        }
+    }
+
+    public function setFeaturedMetaValue($value)
+    {
+        if ($value) {
+            $this->featuredMetaValue = $value;
+        }
+    }
+
+    protected function generateWordPressQuery()
+    {
+        $args = array(
+            'post_type' => array_get($this->options, 'post_type', 'post'),
+            'posts_per_page' => array_get($this->options, 'posts_per_page', 10),
+        );
+
+        if ($this->featuredMetaKey) {
+            $featuredMetaQuery = array(
+                'key' => $this->featuredMetaKey,
+            );
+            if (empty($this->featuredMetaValue)) {
+                $featuredMetaQuery['compare'] = 'EXISTS';
+            } else {
+                $featuredMetaQuery['value'] = $this->featuredMetaValue;
+                $featuredMetaQuery['compare'] = '=';
+            }
+            $args['meta_query'][] = $featuredMetaQuery;
+        }
+
+        return new WP_Query($args);
+    }
 
     public function render()
     {
+        $postLayoutManager = PostLayoutManager::getInstance(TemplateLoader::getTemplateEngine());
+        if (!$postLayoutManager) {
+            return;
+        }
+
+        $layout = $postLayoutManager->createLayout(
+            array_get($this->options, 'layout', Card::LAYOUT_NAME),
+            $this->generateWordPressQuery()
+        );
+        $layout->setOptions($this->layoutOptions);
+
+        return $layout->render(false);
     }
 }
--- src/Renderers/PostsTabsRenderer.php
+++ src/Renderers/PostsTabsRenderer.php
@@ -3,4 +3,15 @@ namespace Jankx\Widget\Renderers;
 
 class PostsTabsRenderer extends Base
 {
+    protected $options = array(
+        'post_type' => 'post'
+    );
+    protected $layoutOptions = array(
+        'columns' => 4,
+    );
+
+
+    public function render()
+    {
+    }
 }
